2d373c5dafa3409970c2c785c4875d27f9857f9d
Axel Zaid Vazquez Enriquez <zedarkhunter@gmail.com>
2023-08-07 11:09:23 -0600

    Se añadio un nuevo archivo de vite al proyecto

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// este archivo es indispensable para que el proyecto se ejecute bien en vite y todas sus caracteristicas
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+
+    base:'/Curso_Blackjack/',
+});
